fa1ae11c346eebec034670928fa793cb3808f26f
Shukuruloh <shukurxondev@gmail.com>
2025-12-23 17:47:57 +0500

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import { ChildProps } from '@/types'
-import './globals.css'
-
 import SessionProvider from '@/components/providers/session-provider'
 import Navbar from '@/components/shared/navbar'
 import { Toaster } from '@/components/ui/sonner'
+import { ChildProps } from '@/types'
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
+import NextTopLoader from 'nextjs-toploader'
 import { FC } from 'react'
+import './globals.css'
 
 const montserrat = Montserrat({
 	weight: ['400', '500', '600', '700', '800', '900'],
@@ -25,6 +25,7 @@ const RootLayout: FC<ChildProps> = ({ children }) => {
 			<html lang='en'>
 				<body className={`${montserrat.className} antialiased`}>
 					<Navbar />
+					<NextTopLoader showSpinner={false} />
 					<main className='container max-w-6xl mt-24'>{children}</main>
 					<Toaster position='bottom-right' />
 				</body>
